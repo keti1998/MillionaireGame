e40de7316eb0fdc171d04e3c8bc8365d47f8ed75
Кетеван Дзебниаури <dzebniauriketi@gmail.com>
2017-11-28 19:32:12 +0300

    Added  Classes and Pages

---
+++ MillionaireGame.UI/Pages.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillionaireGame.UI
+{
+    static class Pages
+    {
+        private static AdminPage _adminPage = new AdminPage();
+        public static AdminPage AdminPage
+        {
+            get
+            {
+                return _adminPage;
+            }
+        }
+
+        private static AuthorizationPage _authPage = new AuthorizationPage();
+        public static AuthorizationPage AuthorizationPage
+        {
+            get
+            {
+                return _authPage;
+            }
+        }
+
+        private static RegistrationPage _regPage = new RegistrationPage();
+        public static RegistrationPage RegistrationPage
+        {
+            get
+            {
+                return _regPage;
+            }
+        }
+
+        private static GamePage _gamePage = new GamePage();
+        public static GamePage GamePage
+        {
+            get
+            {
+                return _gamePage;
+            }
+        }
+        
+
+    }
+}
---
+++ MillionareGame.Data/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillionareGame.Data
+{
+    public class Person
+    {
+        private string _login, _password;
+        public string Login
+        {
+            get { return _login; }
+            set { _login = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public Person(string login, string password)
+        {
+            _password = password;
+            _login = login;
+        }
+    }
+}
